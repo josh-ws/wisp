377de0fc2539556bc3ebab42671a3b1916df5286
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-07-06 23:07:42 +0100

    basic lexing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+#[derive(Debug, Clone, PartialEq)]
+enum Token {
+    LParen,
+    RParen,
+    Atom(String),
+}
+
+fn lex(src: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut chars = src.chars().peekable();
+    while let Some(&c) = chars.peek() {
+        match c {
+            ' ' | '\t' | '\n' | '\r' => {
+                chars.next();
+            }
+            '(' => {
+                chars.next();
+                tokens.push(Token::LParen);
+            }
+            ')' => {
+                chars.next();
+                tokens.push(Token::RParen);
+            }
+            _ => {
+                let mut s = String::new();
+                while let Some(&c) = chars.peek() {
+                    if c.is_whitespace() || "()".contains(c) {
+                        break;
+                    }
+                    s.push(c);
+                    chars.next();
+                }
+                tokens.push(Token::Atom(s));
+            }
+        }
+    }
+    tokens
+}
+
 fn main() {
-    println!("Hello, world!");
+    let src = "(define (square x) (* x x)) (print (square 5))";
+
+    println!("{}\n", src);
+
+    let out = lex(src);
+    for token in out {
+        println!("{:?}", token);
+    }
 }
