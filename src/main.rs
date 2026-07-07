7ffeb44ee0a96a0c800dbc227faff836731ec64c
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-07-07 09:12:01 +0100

    basic parsing

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,77 @@ fn lex(src: &str) -> Vec<Token> {
     tokens
 }
 
+#[derive(Debug, Clone)]
+enum Sexp {
+    Atom(String),
+    List(Vec<Sexp>),
+}
+
+struct Parser {
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, pos: 0 }
+    }
+
+    fn peek(&self) -> Option<&Token> {
+        self.tokens.get(self.pos)
+    }
+
+    fn next(&mut self) -> Option<Token> {
+        let t = self.tokens.get(self.pos).cloned();
+        self.pos += 1;
+        t
+    }
+
+    fn parse(&mut self) -> Result<Sexp, String> {
+        let tok = self.next().ok_or("unexpected eof")?;
+        match tok {
+            Token::LParen => self.parse_list(),
+            Token::RParen => Err("unexpected ')'".to_string()),
+            Token::Atom(a) => Ok(Sexp::Atom(a)),
+        }
+    }
+
+    fn parse_list(&mut self) -> Result<Sexp, String> {
+        let mut items = Vec::new();
+        loop {
+            match self.peek() {
+                Some(Token::RParen) => {
+                    self.next();
+                    return Ok(Sexp::List(items));
+                }
+                None => return Err("unexpected eof".to_string()),
+                _ => items.push(self.parse()?),
+            }
+        }
+    }
+}
+
+fn read(src: &str) -> Result<Vec<Sexp>, String> {
+    let tokens = lex(src);
+    let mut parser = Parser::new(tokens);
+    let mut exprs = Vec::new();
+    while parser.peek().is_some() {
+        exprs.push(parser.parse()?);
+    }
+    Ok(exprs)
+}
+
 fn main() {
     let src = "(define (square x) (* x x)) (print (square 5))";
 
     println!("{}\n", src);
 
-    let out = lex(src);
-    for token in out {
-        println!("{:?}", token);
+    match read(src) {
+        Err(e) => eprintln!("parse error: {}", e),
+        Ok(s) => {
+            for expr in s {
+                println!("{:?}", expr)
+            }
+        }
     }
 }
