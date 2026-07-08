77eb366fdb6eb2ef31e0551f4a523e9efcf93d60
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-07-08 21:32:47 +0100

    add define and if

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,5 +1,11 @@
 use crate::{env::Env, value::Value};
 
+// quote, cond, lambda, define
+// atom, eq, car, cdr, cons
+// +, *, -, /
+// and, or, not
+// https://leinonen.ninja/posts/building-lisp-from-the-ground-up
+
 fn add(args: &[Value]) -> Result<Value, String> {
     let mut sum = 0.0;
     for arg in args {
--- src/eval.rs
+++ src/eval.rs
@@ -26,11 +26,42 @@ fn eval_atom(env: &Env, atom: &str) -> Result<Value, String> {
     }
 }
 
+fn eval_define(env: &mut Env, args: &[Sexp]) -> Result<Value, String> {
+    match args {
+        [Sexp::Atom(name), v] => {
+            let res = eval(env, v)?;
+            env.define(name.clone(), res.clone());
+            Ok(res)
+        }
+        _ => Err("syntax: (define key value)".into()),
+    }
+}
+
+fn eval_if(env: &mut Env, args: &[Sexp]) -> Result<Value, String> {
+    match args {
+        [cond, then, els] => match eval(env, cond)? {
+            Value::Bool(false) => eval(env, els),
+            _ => eval(env, then),
+        },
+        _ => Err("syntax: (if cond then else)".into()),
+    }
+}
+
 pub fn eval(env: &mut Env, expr: &Sexp) -> Result<Value, String> {
     match expr {
         Sexp::Atom(a) => eval_atom(env, a),
         Sexp::List(items) => {
             let (head, args) = items.split_first().ok_or("empty list")?;
+
+            // Special forms
+            if let Sexp::Atom(sym) = head {
+                match sym.as_str() {
+                    "define" => return eval_define(env, args),
+                    "if" => return eval_if(env, args),
+                    _ => {}
+                }
+            }
+
             let func = eval(env, head)?;
             let argv: Vec<Value> = args
                 .iter()
