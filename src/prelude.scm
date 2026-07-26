4cd08ab5238254e09e4542bcc719a4e38444bb12
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-07-26 17:42:07 +0100

    add utility math functions to the prelude

--- src/prelude.scm
+++ src/prelude.scm
@@ -1,3 +1,18 @@
+(define > (lambda (a b) (< b a)))
+(define <= (lambda (a b) (not (< b a))))
+(define >= (lambda (a b) (not (< a b))))
+
+(define zero? (lambda (x) (= x 0)))
+(define positive? (lambda (x) (< 0 x)))
+(define negative? (lambda (x) (< x 0)))
+
+(define abs (lambda (x) (if (negative? x) (- 0 x) x)))
+(define square (lambda (x) (* x x)))
+(define cube (lambda (x) (* x (square x))))
+
+(define max (lambda (a b) (if (< a b) b a)))
+(define min (lambda (a b) (if (< a b) a b)))
+
 (define map
   (lambda (f xs)
     (if (null? xs) '() (cons (f (car xs)) (map f (cdr xs))))))
