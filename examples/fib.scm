bafdfdff813481025a1578216969f476866611e9
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-08-02 21:05:23 +0100

    add missed example

---
+++ examples/fib.scm
@@ -0,0 +1,13 @@
+(define fib
+  (lambda (n)
+    (if (< n 2)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))
+
+(define range
+  (lambda (a b)
+    (if (< b a)
+        '()
+        (cons a (range (+ a 1) b)))))
+
+(map fib (range 1 20))
